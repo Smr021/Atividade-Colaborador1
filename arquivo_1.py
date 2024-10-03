0077a1dc48bc43c75e7eac517b9e1bd46df685d6
سمير <161032439+Smr021@users.noreply.github.com>
2024-10-03 13:34:36 +0000

    Arquivo_1.

--- arquivo_1.py
+++ arquivo_1.py
@@ -1,2 +1,8 @@
 print('Colaborador 1 = Samir Nenem')
-# git pull = atualiza o repositorio
+# git pull   = atualiza o repositorio e traz as atualizaçoes
+# git status = verifica os arquivos se precisam ser salvos.
+# git add .
+# git commit -m "Arquivo_1." = manda o codigo para salvar
+# git push = mandar para o repositorio remoto
+
+
