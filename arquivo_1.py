2d3132d15696c5e207a05e54db34b1920f08da3e
سمير <161032439+Smr021@users.noreply.github.com>
2024-10-03 21:24:06 +0000

    Salvar

--- arquivo_1.py
+++ arquivo_1.py
@@ -5,4 +5,17 @@ print('Colaborador 1 = Samir Nenem')
 # git commit -m "Arquivo_1." = manda o codigo para salvar
 # git push = mandar para o repositorio remoto
 
+# Resolução de conflito 
+# git rebase                   = mostra o conflito
 
+# apague as linhas indesejaveis
+
+# git add .
+
+# git commit -m "Mensagem"
+
+# git rebase --continue          = refaz a base
+
+# git pull
+
+# git push
